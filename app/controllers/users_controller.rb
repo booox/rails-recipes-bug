5b31093896027af49a666c917168602d470b9b5f
booox <booox@users.noreply.github.com>
2017-05-03 05:50:48 +0800

    done user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
     @user = current_user
-    @user.create_profile unless @user.profile
+    # @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
+    # @user = current_user
 
     if @user.update(user_params)
       flash[:notice] = "修改成功"
@@ -18,6 +22,12 @@ class UsersController < ApplicationController
   end
 
   protected
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+
+  end
 
   def user_params
     params.require(:user).permit(:time_zone,
